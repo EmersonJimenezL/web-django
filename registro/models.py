d430eeefc66f91b18bef53ce5cfd8bb0e962fbad
Emerson Jimenez <emersonnicolas.jl@gmail.com>
2024-06-15 01:58:58 -0400

    DjangoWeb models

--- registro/models.py
+++ registro/models.py
@@ -10,7 +10,9 @@ class Cliente(models.Model):
     nombre = models.CharField(max_length=50, blank=False, null=False)
     apellido = models.CharField(max_length=50, blank=False, null=False)
     email = models.CharField(max_length=50, blank=False, null=False)
-    direccion = models.CharField(max_length=50, blank=False, null=False)   
+    direccion = models.CharField(max_length=50, blank=False, null=False)  
+    region = models.CharField(max_length=50, blank=False, null=False)  
+    comuna = models.CharField(max_length=50, blank=False, null=False)  
     contrasenna = models.CharField(max_length=100, blank=False, null=False)
 
 
