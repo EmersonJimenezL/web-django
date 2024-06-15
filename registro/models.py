ec8385f0e033002e83a81b916bae7525be1a6c77
Emerson Jimenez <emersonnicolas.jl@gmail.com>
2024-06-14 21:51:28 -0400

    django database (regiones y comunas obtener datos)

--- registro/models.py
+++ registro/models.py
@@ -6,8 +6,13 @@ from django.db import models
 from .models import models
 
 class Cliente(models.Model):
-    nombreUsuario = models.CharField(max_length=30, unique=True) 
-    contrasenna = models.CharField(max_length=100)
+    rutCliente = models.CharField(db_column='rut',max_length=11 ,primary_key=True)
+    nombre = models.CharField(max_length=50, blank=False, null=False)
+    apellido = models.CharField(max_length=50, blank=False, null=False)
+    email = models.CharField(max_length=50, blank=False, null=False)
+    direccion = models.CharField(max_length=50, blank=False, null=False)   
+    contrasenna = models.CharField(max_length=100, blank=False, null=False)
+
 
 def __str__(self):
-    return str(self.nombreUsuario)+" "+str(self.contrasenna)
+    return str(self.rutCliente)
--- registro/views.py
+++ registro/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def registroView(request):
-    return render(request, 'registro.html')
+    if (request.method == 'POST'):
+        ## agregan elemento
+        print(request.POST["nombre"])
+        print(request.POST["region"])
+        return render(request, 'registro.html')
+    else:
+        ## aca obtiene solamente el formulario
+        print(request)
+        return render(request, 'registro.html')
