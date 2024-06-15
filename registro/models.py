d409b954b95bea309f4531542360873a75bfc521
Emerson Jimenez <emersonnicolas.jl@gmail.com>
2024-06-14 20:01:28 -0400

    django database

--- registro/models.py
+++ registro/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+from .models import models
+
+class Cliente(models.Model):
+    nombreUsuario = models.CharField(max_length=30, unique=True) 
+    contrasenna = models.CharField(max_length=100)
+
+def __str__(self):
+    return str(self.nombreUsuario)+" "+str(self.contrasenna)
